3d88f0810a605e9fc3913be7cd232d4bfcfab5b3
rakinplaban <rakinshahriar54@gmail.com>
2022-12-09 11:21:04 +0600

    App started successfully.

--- Batch_Email/urls.py
+++ Batch_Email/urls.py
@@ -18,5 +18,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('mailapp/',include('mailapp.urls')),
+    path('',include('mailapp.urls')),
 ]
