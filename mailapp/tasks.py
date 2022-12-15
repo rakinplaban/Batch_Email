94bcf8ea0a36482d4cd135bc39d67dd345b797bf
rakinplaban <rakinshahriar54@gmail.com>
2022-12-15 21:16:02 +0600

    Completed.

--- mailapp/tasks.py
+++ mailapp/tasks.py
@@ -1,5 +1,6 @@
 from celery import shared_task
-from django.core.mail import send_mail
+# from django.core.mail import send_mail
+from django.core.mail import EmailMultiAlternatives
 from .models import User
 
 @shared_task(bind=True)
@@ -8,8 +9,11 @@ def send_mail_func(self,subject,body,sender):
     receipents = []
     for user in users:
         receipents.append(user.email)
-    send_mail(
-        subject,body,sender,receipents, 
-        fail_silently=False
-    )
+    # send_mail(
+    #     subject,body,sender,receipents, 
+    #     fail_silently=False
+    # )
+    msg = EmailMultiAlternatives(subject,body,sender,receipents)
+    msg.attach_alternative(body, "text/html")
+    msg.send()
     return "Done!"
