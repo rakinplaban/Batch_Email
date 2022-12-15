14e2568c5d86496435e0c42c9bfd0ca9c4b37f83
rakinplaban <rakinshahriar54@gmail.com>
2022-12-15 18:50:41 +0600

    ----

--- mailapp/models.py
+++ mailapp/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.db.models.deletion import CASCADE
 from ckeditor.fields import RichTextField
+from ckeditor_uploader.fields import RichTextUploadingField
 from datetime import datetime
 # Create your models here.
 
@@ -12,6 +13,7 @@ class User(AbstractUser):
 class Email(models.Model):
     subject = models.CharField(max_length=100)
     body = RichTextField()
+    body_upload = RichTextUploadingField(blank=True,null=True)
     sender = models.ForeignKey(User, on_delete=CASCADE,related_name='sender')
     timestamp = datetime.now()
     
